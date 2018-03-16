7ec46e4c89aa553520efa2c4c8f0be8cc8bb40fe
Burak Alp <burakalp17@gmail.com>
2018-03-16 15:56:26 +0300

    adding files

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    cssnano = require('gulp-cssnano');
+    cssnano = require('gulp-cssnano'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    plumber = require('gulp-plumber');
 
 var scssFile = 'app/scss/allinone.scss';
 var cssDir = 'app/css/';
 
 gulp.task('sass',function(){
   return gulp.src(scssFile)
+        .pipe(plumber())
         .pipe(sass())
         .pipe(cssnano())
         .pipe(gulp.dest(cssDir))
