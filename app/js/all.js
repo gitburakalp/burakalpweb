863b1acca3f8973b84a225db370bfc84449fe0d7
Burak Alp <burakalp17@gmail.com>
2018-03-19 17:30:05 +0300

    Adding js files

---
+++ app/js/all.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+  $('.loader').delay(500).fadeOut();
+  $('.wrapper').delay(1000).fadeIn(500);
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,35 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    cssnano = require('gulp-cssnano'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    plumber = require('gulp-plumber');
+  sass = require('gulp-sass'),
+  cssnano = require('gulp-cssnano'),
+  uglify = require('gulp-uglify'),
+  concat = require('gulp-concat'),
+  plumber = require('gulp-plumber');
 
 var scssFile = 'app/scss/allinone.scss';
 var cssDir = 'app/css/';
+var jsFiles = ['node_modules/jquery/dist/jquery.js','app/js/all.js'];
 
-gulp.task('sass',function(){
+gulp.task('sass', function() {
   return gulp.src(scssFile)
-        .pipe(plumber())
-        .pipe(sass())
-        .pipe(cssnano())
-        .pipe(gulp.dest(cssDir))
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(cssnano())
+    .pipe(gulp.dest(cssDir))
 });
 
-gulp.task('watch',function(){
-  gulp.watch('app/scss/**/*.scss',['sass']);
+gulp.task('js:min', function() {
+  return gulp.src(jsFiles)
+    .pipe(concat('all.min.js'))
+    .pipe(uglify({
+       mangle: {
+          toplevel: true
+        }
+     }))
+    .pipe(gulp.dest('app/js/'));
+})
+
+gulp.task('watch', function() {
+  gulp.watch('app/scss/**/*.scss', ['sass']);
 });
 
-gulp.task('default', ['sass','watch']);
+gulp.task('default', ['sass', 'watch']);
